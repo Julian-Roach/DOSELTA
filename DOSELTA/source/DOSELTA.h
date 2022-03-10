98990f1a74f534aec4bea9d1b37b395c2a97cb35
Mr. Roach <julianroach.dev@gmail.com>
2022-03-10 20:23:35 +0100

    getting ready for the first release

--- DOSELTA/source/DOSELTA.h
+++ DOSELTA/source/DOSELTA.h
@@ -1,22 +1,21 @@
-/******************************************************************************
+/**************************************************************************************************
 DOSELTA - Data Oriented, Small and Efficient c++ Library for Task Automation.
 
-The Description:
-DOSELTA is a library helping you to write applications for your simple tasks.
-You can quickly write an app that will go through some data, process it, and
-give you some output, if you wish. DOSELTA is great for creating little
-projects requiring data storage, such as diaries, or lists of ideas.
+The Description: DOSELTA is a library helping you to write applications for your simple tasks. You
+can quickly write an app that will go through some data, process it, and give you some output, if
+you wish. DOSELTA is great for creating little projects requiring data storage, such as diaries,
+or lists of ideas.
 
-Be careful though - don't delete the .doselta files! Otherwise you will lose
-all of that data permamently.
+Be careful though - don't delete the .doselta files! Otherwise you will lose all of that data
+permamently.
 
-Actual Purpose:
-Basically, I created this library just for myself; I just needed to make simple
-little applications, (such as diaries, TODO lists, password storages, etc. ) so
-I created this "shortcut" for them. This project isn't meant to be used by
-anyone, but if you like it, and you would like to "upgrade" it a little, feel
-free to pull request your changes!
-*******************************************************************************/
+Reason: Basically, I created this library just for myself; I just needed to make simple little
+applications, (such as diaries, TODO lists, password storages, etc. ) so I created this "shortcut"
+for them.
+
+This project isn't meant to be used by anyone, but if you like it, and you would like to "upgrade"
+it a little, feel free to pull request your changes!
+**************************************************************************************************/
 
 #pragma once
 
